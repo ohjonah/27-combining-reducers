774086eed036febd14c0c454f14f593a95a90033
ohjonah <ohjonah@gmail.com>
2017-08-30 08:42:56 -0700

    Store accepts Reducer Index

--- src/reducer/expense-reducer.js
+++ src/reducer/expense-reducer.js
@@ -6,10 +6,13 @@ export default (state=initialState, action) => {
   switch(type) {
     case 'EXPENSE_CREATE':
       return [...state, payload];
+
     case 'EXPENSE_UPDATE':
       return state.map(expense => expense.id === payload.id ? payload : expense);
+
     case 'EXPENSE_DELETE':
       return state.filter(expense => expense.id !== payload.id)
+
     default:
       return state;
   }
