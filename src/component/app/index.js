a560acdacafe4a906d484a3abfa96feaa3615ff6
ohjonah <ohjonah@gmail.com>
2017-08-31 16:09:14 -0700

    Adds Middleware Component

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 
 import DashboardContainer from '../dashboard-container';
 import reducer from '../../reducer';
+import reporter from '../../lib/redux-reporter.js';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(reporter));
 
 class App extends React.Component {
 
   componentDidMount() {
-    store.subscribe(() => {
-      console.log('__STATE__:', store.getState())
-    });
-
     store.dispatch({ type: null });
   }
 
